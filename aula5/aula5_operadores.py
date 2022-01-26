a46039cfb5cc931fbeed88e997dd7b809c2b8794
Brunno Silva <brunnoqsilva@gmail.com>
2022-01-26 00:58:02 -0300

    Exercícios com operadores matemáticos.

---
+++ aula5/aula5_operadores.py
@@ -0,0 +1,20 @@
+"""
++ SOMA
+- SUBTRAÇÃO
+* MULTPLICAÇÃO
+/ DIVISAO
+// DIVISAO INTEIRA (Arrendodar
+** Potênciação
+% resto de divisão
+() precedência das operações
+Documentação completa --> https://docs.python.org/3/reference/expressions.html#operator-precedence
+"""
+print("SUM: ", 10 + 10)
+print("SUBTRACTION: ", 10 - 10)
+print("Multiplication: ", 10 * 10)
+print("DIVISION: ", 10 / 10)
+print("WHOLE DIVISION: ", 15 // 2)
+print("Potentiation: ", 10 ** 2)
+print("precedence real: ", 5 + 2 * 10)
+print("precedence fake: ", (5 + 2) * 10)
+print("repeat 100x 'a': ", 100 * "A")  # Muito louco
