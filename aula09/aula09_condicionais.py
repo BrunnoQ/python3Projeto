0888d73cfb5dfe2c45631fc041416ffd7cf91749
Brunno Silva <brunnoqsilva@gmail.com>
2022-01-26 22:17:53 -0300

    [AULA 09] Operadores lógicos e condicionais.

---
+++ aula09/aula09_condicionais.py
@@ -0,0 +1,45 @@
+"""
+B ABA nem vou perder muito tempo.
+
+Operadores lógicos:
+== IGUAL
+> MAIOR
+>= MAIOR IGUAL
+<= MENOR IGUAL
+!= DIFERENTE
+
+and e
+or ou
+not negação
+in esta em
+not in nao esta em
+
+"""
+if True:
+    print("Roda a roda jequeti!")
+# elif False:
+#    print("Roda a roda jequeti!")
+# else:
+#    print("Roda a roda jequeti!")
+
+name = "Brunno Silva"
+
+expression = 1 == 2
+expression2 = 1 != 2
+expression3 = 1 <= 2
+expression4 = 1 >= 2
+expression5 = 1 >= 2 or 1 <= 2
+expression6 = 1 == 1 and 1 <= 2
+expression7 = not 1 == 1 and 1 <= 2
+expression8 = "u" in name
+expression9 = "x" in name
+
+print(expression)
+print(expression2)
+print(expression3)
+print(expression4)
+print(expression5)
+print(expression6)
+print(expression7)
+print(expression8)
+print(expression9)
