9853096f2ae5d24da7c2ecfac0ec8eea5cd7161b
Brunno Silva <brunnoqsilva@gmail.com>
2022-01-31 21:55:47 -0300

    [aula16] Mais básico de função, um pouco de TUPLAS

--- aula16/aula16_funcoes.py
+++ aula16/aula16_funcoes.py
@@ -14,3 +14,24 @@ teacher()
 pigeon("Fogo na bomba", "Brunno")
 jaguar()
 jaguar(name='Zezinho')  # Com parâmetros nomeados é possível mudar a ordem de envio deles.
+
+
+def send_print(var):
+    print(f'send_print printou {var}')
+
+
+def dumb():
+    return send_print
+
+
+var = dumb()
+
+print(dumb()("Locura chamei outra função"))  # Isso acontece pois dumb retorna a função
+print(id(var), id(dumb()), id(send_print))  # Mesmo endereço de memória
+
+#TUPLA
+def dumb2():
+    return 'Brenda', 'Michelle'
+
+
+print(type(dumb2()))
