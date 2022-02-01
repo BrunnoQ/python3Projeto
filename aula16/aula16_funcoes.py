095ec3373cde3eed1071f9385f9ceac0d1b8d158
Brunno Silva <brunnoqsilva@gmail.com>
2022-01-31 22:31:43 -0300

    [aula16] Mais básico de função, um pouco de TUPLAS

--- aula16/aula16_funcoes.py
+++ aula16/aula16_funcoes.py
@@ -29,9 +29,34 @@ var = dumb()
 print(dumb()("Locura chamei outra função"))  # Isso acontece pois dumb retorna a função
 print(id(var), id(dumb()), id(send_print))  # Mesmo endereço de memória
 
-#TUPLA
+
+# TUPLA
 def dumb2():
     return 'Brenda', 'Michelle'
 
 
 print(type(dumb2()))
+
+"""
+Funções em Python - *args **kwargs
+kwargs siginifica --> keyword arguments, e somente por convenção se chama assim, é representado por dois '**'
+     args é representado por '*' *args **kwargs
+"""
+
+
+# args é UMA TUPLA
+def dummy(*args, **kwargs):
+    print(args)
+    print(len(args))
+    print(type(args))
+    print(type(kwargs))
+    if kwargs:
+        print(kwargs['name'], kwargs['surname'])
+        print(kwargs.get('nome'))  # Dessa maneira é melhor que nao tomo NULLPOINTER
+
+
+dummy(1, 2, 3, 45, 6)
+listU = [1, 5, 14, 48, 6]
+dummy(listU)  # Lista sem DESEMPACOTAMENTO
+dummy(*listU)  # Lista com DESEMPACOTAMENTO
+dummy('Veransca', 'Rancher', 'Mandrião', 'servente', name='Brunno', surname='Silva')
