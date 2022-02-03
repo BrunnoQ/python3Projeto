8a0104cf9d71030209da04fdf4e480375f83acc2
Brunno Silva <brunnoqsilva@gmail.com>
2022-02-03 00:06:36 -0300

    [aula22] Decoradores

---
+++ aula22/aula22_decoradores.py
@@ -0,0 +1,49 @@
+"""
+Aprender a criar funções decoradoras e decoradas
+@static por exemplo
+"""
+
+
+def master(funcao):
+    def slave(*args, **kwargs):
+        print('Estou decorada baby!')
+        funcao(*args, **kwargs)  # Adicionando o () digo para o interpretador que isso é uma função! Tipagem dinâmica
+        # mata!
+
+    return slave
+
+
+# PQP aí já demais, função decorada! TOPPP
+@master
+def fala_oi():
+    print('Oi')
+
+
+@master
+def outra_funcao(msg):
+    print(msg)
+
+
+# Decorada na "mão"
+# fala_oi = master(fala_oi)  # Variavel é uma função
+
+fala_oi()
+outra_funcao('Ola senhora')
+
+"""
+Problemas dos parâmetros mutáveis (LISTAS e DICIONARIOS por exemplo) em funções
+"""
+
+
+# o problema esta em lista , pois o interpretador do python cria uma UNICA LISTA na memória
+# e sempre que o metodo for chamado com esse valor default, ela sera ALTERADA e nao um novo criado!
+def lista_de_clientes(clientes_novos, lista=[]):
+    lista.extend(clientes_novos)  # vai sempre engordando a mesma lista.
+    return lista
+
+
+clientes1 = lista_de_clientes(['MÁRCIA', 'ANA', 'FELIPA'])  # lista DEFAULT
+clientes2 = lista_de_clientes(['ENRICA', 'LUCIA', 'VERA'])  # lista DEFAULT
+
+print(clientes1)
+print(clientes2)  # LISTA MERGEADA
