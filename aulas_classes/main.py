56c91a2fa4105a5d86f08d77330ffe72da7db53a
Brunno Silva <brunnoqsilva@gmail.com>
2022-02-14 22:42:55 -0300

    [Objetos] Um poouco mais de classes em python!

---
+++ aulas_classes/main.py
@@ -0,0 +1,14 @@
+from pessoa import Pessoa
+
+p1 = Pessoa(nome='Carla', idade=26)
+p1.comer('Amora')
+p1.comer('Amora')
+
+print(Pessoa.ano_atual)
+print(p1.get_ano_nascimento())
+p2 = Pessoa.por_ano_nacimento('Ramon', 1986)
+print(type(p2))
+print(Pessoa.por_ano_nacimento('Ramon', 1986))
+print(f'Gerando ID {Pessoa.gera_id()}')
+p3 = Pessoa('Gertrudes', '1986')
+print(f'IDade {p3.idade}')
---
+++ aulas_classes/pessoa.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from random import randint
+
+
+class Pessoa:
+    ano_atual = int(datetime.strftime(datetime.now(), '%Y'))  # atributo static
+
+    def __init__(self, nome, idade, comendo=False, falando=False):
+        self.nome = nome
+        self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+
+    # Getter por convenção o "_" vai na frente
+    @property
+    def nome(self):
+        return self._nome
+
+    # Setter
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    @property
+    def idade(self):
+        return self._idade
+
+    # Setter
+    @idade.setter
+    def idade(self, idade):
+        if isinstance(idade, str):
+            print('wtf??')
+        else:
+            print(type(idade))
+            self._idade = idade
+
+    def comer(self, alimento):
+        if self.comendo:
+            print(f'{self.nome}, esta comendo')
+
+        self.comendo = True
+        print(f'{self.nome}, esta comendo {alimento}')
+
+    def get_ano_nascimento(self):
+        return self.ano_atual - self.idade
+
+    # Metodo da classe, retorna um OBJETO desta classe CLS poderia ser qualquer nome, assim como SELF
+    @classmethod
+    def por_ano_nacimento(cls, nome, ano_nascimento):
+        idade = cls.ano_atual - ano_nascimento
+        return cls(nome, idade)
+
+    # Diferentemente de um class method esse camarada não precisa de nenhuma referencia 'CLS'
+    @staticmethod
+    def gera_id():
+        rand = randint(10000, 19999)
+        return rand
+
