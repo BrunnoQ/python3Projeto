a1034bf8389c137282ab05a24f425d9d01ecaf9c
Brunno Silva <brunnoqsilva@gmail.com>
2022-05-19 22:03:33 -0300

    Mais um commit para o trace de produtividade ficar bonito no GITHUB :-)

--- aula_funcoes/aula3_string.py
+++ aula_funcoes/aula3_string.py
@@ -1,3 +1,4 @@
 """Tanto faz usas aspas simples ou duplas para definir uma sTRING!"""
 print("This is an 'String'")  # Scape of characters
 print('This is an "String" too!')
+#Passando para relembrar, ja que faz tempo que nõa mexo... maravilha!
