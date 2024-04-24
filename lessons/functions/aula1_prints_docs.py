a40c05b9e1afe71329d5c78b1d4532c9cf1b7d4a
Brunno Silva <brunnoqsilva@gmail.com>
2024-04-23 23:22:58 -0300

    Add code documentation for print statements in aula1_prints_docs.py

--- lessons/functions/aula1_prints_docs.py
+++
@@ -1,9 +0,0 @@
-'''Server para documentar o código, 
-para que outros programadores possam entender o que o código faz, como ele funciona e como ele deve ser usado.'''
-print("This is an 'String'")  # Scape of characters
-print('This is an "String" too!') # Scape of characters
-
-"""
-Docstring é uma string que aparece no início de um módulo, função, classe ou método
-"""
-print("This is an", sum([1, 2, 3, 4, 5]))  # Somas os números da lista
