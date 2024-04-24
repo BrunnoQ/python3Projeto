37f9ee86f303351edeb2b49fa1d8d40720f67319
Brunno Silva <brunnoqsilva@gmail.com>
2024-04-23 22:29:23 -0300

    Fix import statement in aula_funcoes_old/aula3_string.py

--- aula_funcoes_old/aula3_string.py
+++ aula_funcoes_old/aula3_string.py
@@ -1,7 +1,7 @@
+import sys
 """Tanto faz usas aspas simples ou duplas para definir uma sTRING!"""
 print("This is an 'String'")  # Scape of characters
 print('This is an "String" too!')
 #Passando para relembrar, ja que faz tempo que nõa mexo... maravilha!
 #Get version of Python
-import sys
 print(sys.version)
